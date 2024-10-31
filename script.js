ca6c0204715aab6247accccd30d6a4b634ffeb2b
Razvan Ciocan <rzv.c93@outlook.com>
2024-10-31 17:02:00 +0000

    Renaming the repository and  fix the spacing inside the code

--- script.js
+++ script.js
@@ -12,33 +12,24 @@ function setBtn(buttons) {
     }); 
 }
 
-
-
 function selectButton() {
     const buttons = document.querySelectorAll(".card button"); 
     setBtn(buttons); 
 }
 
-
-
-
 function generateButtons() {
     const noInput = document.getElementById('noInput');
     const noOfBtn = parseInt(noInput.value);
-
     if (isNaN(noOfBtn) || noOfBtn <= 0) { 
         alert("Te rog introdu un număr valid.");
         return;
     }
-
     const existingContainer = document.querySelector('.buttons-container');
-    
     if (existingContainer) {
         existingContainer.remove();
     }
     const container = document.createElement('div');
     container.classList.add('buttons-container');
-    
     for (let i = 0; i < noOfBtn; ++i) {
         const btn = document.createElement('button');
         btn.textContent = 'Chance ' + (i + 1);
@@ -53,4 +44,3 @@ function generateButtons() {
 }
 selectButton();
 document.getElementById('btnGenerate').addEventListener('click', generateButtons);
-
